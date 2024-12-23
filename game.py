d6591c39cfd0aaa8eadcde0e2466c6cab0bdfb84
Patrick Maloney <boyscoutpatrick813@gmail.com>
2024-12-23 02:05:30 -0600

    Fixed #1 Fixed issue where you could select part of an unavailable sum and lose Updated bot to use cog and to sync the tree in a spot not likely to cause rate limits Set up logging (make sure to set log_level in .env) Added requirements.txt

--- game.py
+++ game.py
@@ -1,13 +1,17 @@
-import numpy as np
-from enum import Enum
-from helpers import SumsTable
 import random
+from enum import Enum
+
+import numpy as np
+
+from helpers import SumsTable
+
 
 class GameState(Enum):
     ROLL = 1
     FLIP_BOXES = 2
     LOSE = 3
     WIN = 4
+
 
 class ShutTheBoxGame:
     def __init__(self, num_faces: int, num_die: int):
@@ -33,7 +37,7 @@ class ShutTheBoxGame:
             for num in possible_sum:
                 # Convert from 1-indexed to 0-indexed
                 # If box is already closed, the sum is not possible
-                if self.boxes_closed[num-1]:
+                if self.boxes_closed[num - 1]:
                     this_sum_possible = False
             if this_sum_possible:
                 any_possible = True
@@ -43,8 +47,9 @@ class ShutTheBoxGame:
     def handle_roll(self):
         if (self.game_state != GameState.ROLL):
             raise ValueError("Incorrect state")
-        
+
         # Roll die
+
         sum = 0
         for _ in range(self.num_die):
             sum += random.randint(1, self.num_faces)
@@ -64,33 +69,40 @@ class ShutTheBoxGame:
 
         # Convert from 1-indexed to 0-indexed
         box_index = box_to_flip - 1
-        
+
         # Flip box and subtract value
         self.boxes_closed[box_index] = True
         self.curr_die_rem -= box_to_flip
-        
-        if not self.__are_sums_possible(self.curr_die_rem):
-            self.game_state = GameState.LOSE
-        elif self.curr_die_rem == 0 and all(self.boxes_closed):
+
+        if self.curr_die_rem == 0 and all(self.boxes_closed):
             self.game_state = GameState.WIN
         elif self.curr_die_rem == 0:
             self.game_state = GameState.ROLL
+        elif not self.__are_sums_possible(self.curr_die_rem):
+            self.game_state = GameState.LOSE
 
     def get_die_remaining(self):
         return self.curr_die_rem
-    
+
     def get_boxes_closed(self):
         return self.boxes_closed
-    
+
+    def get_nums_closed(self) -> list:
+        closed = self.get_boxes_closed()
+        return [x for x in range(1, len(closed) + 1) if closed[x - 1]]
+
     def get_possible_flips(self):
         flips = []
-        for sums in self.sums_table.get_all_sums(self.curr_die_rem):
+        closed_nums = self.get_nums_closed()
+        all_sums = self.sums_table.get_all_sums(self.curr_die_rem)
+        for sums in all_sums:
+            if set(sums).intersection(set(closed_nums)):
+                continue
             for num in sums:
                 if num not in flips and not self.boxes_closed[num - 1]:
                     flips.append(num)
         flips.sort()
         return flips
-    
+
     def get_game_state(self):
         return self.game_state
-    
